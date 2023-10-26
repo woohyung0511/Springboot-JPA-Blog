c0bd86687d6293b8b5841e43106e7da398788eb2
woohyung0511 <erst321@naver.com>
2023-10-27 01:44:36 +0900

    CRUD 테스트, 메인페이지 작업

--- src/main/java/com/cos/blog/model/Reply.java
+++ src/main/java/com/cos/blog/model/Reply.java
@@ -1,5 +1,43 @@
 package com.cos.blog.model;
 
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Reply {
 
+	@Id //Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+	private int id; //시퀀스,auto_increment
+	
+	@Column(nullable = false, length = 200)
+	private String content;
+	
+	@ManyToOne
+	@JoinColumn(name = "boardId")
+	private Board board;
+	
+	@ManyToOne
+	@JoinColumn(name = "userId")
+	private User user;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
 }
--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -4,21 +4,34 @@ import java.sql.Timestamp;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder //빌더 패턴!!
 @Entity // User 클래스가 MySQL에 테이블이 생성이 된다.
+//@DynamicInsert //insert 시 null인 필드를 제외시킨다.
 public class User {
 
 	@Id //Primary Key
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
 	private int id; // 시퀀스,auto_increment
 
-	@Column(nullable = false, length = 30)
+	@Column(nullable = false, length = 30, unique = true)
 	private String username; //아이디
 	
 	@Column(nullable = false, length = 100) //123456  => 해쉬 (비밀번호 암호화)
@@ -26,9 +39,11 @@ public class User {
 	
 	@Column(nullable = false, length = 50)
 	private String email;
-	
-	@ColumnDefault("'user'")
-	private String role; //Enum을 쓰는게 좋다. //admin, user, manager
+
+	//	@ColumnDefault("user")
+	// DB는 RoleType 이라는게 없다.
+	@Enumerated(EnumType.STRING)
+	private RoleType role; //Enum을 쓰는게 좋다. //ADMIN, USER
 	
 	@CreationTimestamp //시간이 자동입력
 	private Timestamp createDate;
