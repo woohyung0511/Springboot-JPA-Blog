20d5a1b1ce9078894d47af44b218f44b800faee8
woohyung0511 <erst321@naver.com>
2023-10-28 23:36:03 +0900

    회원가입,로그인 완료

--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.cos.blog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.cos.blog.model.User;
@@ -8,6 +9,11 @@ import com.cos.blog.model.User;
 // DAO
 // 자동으로 bean 등록이 된다.
 //@Repository //생략 가능하다.
-public interface UserRepository extends JpaRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, Integer> {
+	// JPA Naming 전략
+	// SELECT * FROM user WHERE username = ?1 AND password = ?2;
+	User findByUsernameAndPassword(String username, String password);
 
+//	@Query(value="SELECT * FROM user WHERE username = ?1 AND password = ?2",nativeQuery = true)
+//	User login(String username, String password);
 }
